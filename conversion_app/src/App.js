9c479119b757caf0758ec73dfefc61d557317fde
kylewgunter <80540418+kylewgunter@users.noreply.github.com>
2021-10-06 00:29:13 -0500

    kyle

--- conversion_app/src/App.js
+++ conversion_app/src/App.js
@@ -4,39 +4,43 @@ import InputRow from './components/InputRow';
 import TargetRow from './components/TargetRow';
 import Calculator from './components/Calculator';
 import convert from 'convert-units'
+// import configureMeasurements, { temperature, volume } from 'convert-units';
+
+// const convert = configureMeasurements({
+//   temperature,
+//   volume
+// });
 
 const temperature = convert().possibilities('temperature')
 const volume = convert().possibilities('volume')
 
 function App() {
 
-  const [unitOptions, setUnitOptions] = useState([]);
+  const [unitOptions] = useState([...Object.values(temperature), ...Object.values(volume)]);
   const [fromUnit, setFromUnit] = useState();
   const [toUnit, setToUnit] = useState();
 
-  // console.log(typeof(unitOptions))//object
+  console.log(unitOptions)
 
   useEffect (() => {
-    setUnitOptions([...Object.values(temperature), ...Object.values(volume)]);
-    console.log(typeof(unitOptions))
-    const unit = Object.keys(unitOptions)
-
-    // setFromUnit(Object.values(firstUnit))
-    // setToUnit(Object.values(toUnit))
+    // setUnitOptions([...Object.values(temperature), ...Object.values(volume)])
+    // setFromUnit = () => {
+    
+    // }
   }, [])
 
   return (
     <>
       <h1>Unit Converter</h1>
       <hr></hr>
-      <InputRow 
+      <InputRow
         unitOptions={unitOptions}
       />
       <div>
         <p className="subhead">Convert to</p>
       </div>
       <TargetRow 
-      unitOptions={unitOptions}
+        unitOptions={unitOptions}
       />
       <Calculator />
     </>

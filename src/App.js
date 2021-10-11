5d09040515c156c0d92a3ba3cfa1f51d01d3fed8
kylewgunter <80540418+kylewgunter@users.noreply.github.com>
2021-10-10 19:07:57 -0500

    Kyle Notation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState }from 'react'
 import './App.css';
+
 import InputRow from './components/InputRow';
 import TargetRow from './components/TargetRow';
 import Calculator from './components/Calculator';
+
 import convert from 'convert-units'
 
 const temperature = convert().possibilities('temperature')
 const volume = convert().possibilities('volume')
 
 function App() {
-
+  // state functions that will update with teachers input
   const [unitOptions] = useState([...Object.values(temperature), ...Object.values(volume)]);
   const [startUnit, setStartUnit] = useState(unitOptions[0]);
   const [convertUnit, setConvertUnit] = useState(unitOptions[0]);
@@ -20,6 +22,7 @@ function App() {
     <>
       <h1>Unit Converter</h1>
       <hr></hr>
+      {/* input row that accepts start unit and value when updated as props to components */}
       <InputRow
         unitOptions={unitOptions} 
         selectedUnit={startUnit}
@@ -29,6 +32,8 @@ function App() {
       <div>
         <p className="subhead">Convert to</p>
       </div>
+      {/* target row that accepts start unit and value when updated as props to componeents */}
+
       <TargetRow 
         unitOptions={unitOptions}
         selectedUnit={convertUnit}
